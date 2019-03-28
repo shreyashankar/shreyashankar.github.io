b277c6489cce64ab8ac3bb1d8364b7025233d47a
Shreya <ss.shankar505@gmail.com>
2019-03-28 13:58:24 -0700

    edits

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,13 +18,10 @@ export default class About extends React.Component {
           I'm a masters student at Stanford University studying computer science with a focus in artificial intelligence. I also did my undergrad at Stanford studying computer systems. I've worked at <a href="https://ai.google/research/teams/brain">Google Brain</a>, <a href="https://amplifypartners.com/">Amplify Partners</a>, and <a href="https://www.facebook.com">Facebook</a>.
         </p> 
         <p>
-          During my undergrad, I was involved in <a href="https://cs4good.com/">CS+Social Good</a>, a student organization that focuses on the intersection of technology and social impact. I also helped run a nonprofit called <a href="http://www.sheplusplus.com/">SHE++</a>, an organization that helps to empower underrepresented minorities in technology. I have also been on the organizing team for <a href="https://www.treehacks.com/">TreeHacks</a>, Stanford's largest international hackathon. 
+          I'm interested in machine learning security and AI-inspired art. I care about building a future where AI augments human abilities and isn't easily broken. I'm also passionate about diversity in the tech industry, as I helped run a nonprofit called <a href="http://www.sheplusplus.com/">SHE++</a>, an organization that helps to empower underrepresented minorities in technology.
         </p>
         <p>
-          I'm pretty opinionated about many things involving computers, consumer products, research, and diversity. I try to blog regularly, but I definitely tweet regularly. <a href="https://twitter.com/sh_reya">Follow me on Twitter.</a> 
-        </p>
-        <p>
-          To contact me, please email <a href="mailto:shreya@cs.stanford.edu">shreya@cs.stanford.edu</a>. 
+          To contact me, please email <a href="mailto:shreya@cs.stanford.edu">shreya@cs.stanford.edu</a>. Also, follow me on <a href="https://twitter.com/sh_reya">Twitter.</a> 
         </p>
       </Layout>
     )
