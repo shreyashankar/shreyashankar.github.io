1e67442c72c917759795eff18550d7aa3474d7ef
Shreya Shankar <ss.shankar505@gmail.com>
2021-01-08 18:19:54 -0600

    Change about

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,17 +18,17 @@ export default class About extends React.Component {
         <Links/>
         <div style={{marginBottom: '40px'}}></div>
         <p>
-          I'm Shreya Shankar, a computer scientist living in the Bay Area. I'm interested in making machine learning work in the real world. 
+          I'm Shreya Shankar, a computer scientist living in the Bay Area. I'm interested in making machine learning work in the real world.
         </p>
 
-        <p>  
-          Currently, I work at <a href="https://www.viaduct.ai/">Viaduct</a>. Previously, I did research at <a href="https://ai.google/research/teams/brain">Google Brain</a>, learned about investing with <a href="https://amplifypartners.com/">Amplify Partners</a>, and software engineering at <a href="https://www.facebook.com">Facebook</a>.
-        </p> 
+        <p>
+          Currently, I am taking a break. Previously, I was the first ML engineer at <a href="https://www.viaduct.ai/">Viaduct</a>, did research at <a href="https://ai.google/research/teams/brain">Google Brain</a>, learned about investing with <a href="https://amplifypartners.com/">Amplify Partners</a>, and software engineering at <a href="https://www.facebook.com">Facebook</a>.
+        </p>
         <p>
           I graduated from Stanford University with a B.S. and M.S. in computer science. I concentrated in systems and artificial intelligence respectively. At Stanford, I helped run a nonprofit called <a href="http://www.sheplusplus.com/">SHE++</a>, an organization that helps to empower underrepresented minorities in technology. I also spent a lot of time as a section leader and teaching assistant for <a href="https://cs198.stanford.edu/cs198/">CS198</a>.
         </p>
         <p>
-          To reach me, you can email <a href="mailto:shreya@cs.stanford.edu">shreya@cs.stanford.edu</a>. I am also fairly active on <a href="https://twitter.com/sh_reya">Twitter.</a> 
+          To reach me, you can email <a href="mailto:shreya@cs.stanford.edu">shreya@cs.stanford.edu</a>. I am also fairly active on <a href="https://twitter.com/sh_reya">Twitter.</a>
         </p>
       </Layout>
     )
