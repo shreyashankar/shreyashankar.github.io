432d615f6392383b4702bfb9e365e5aa9b46da41
Shreya Shankar <ss.shankar505@gmail.com>
2022-01-01 18:09:45 -0600

    Change about

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,7 +23,7 @@ export default class About extends React.Component {
         </p>
 
         <p>
-          Currently, I am doing my PhD in the <a href="https://rise.cs.berkeley.edu/" target="_blank">RISELab</a> at UC Berkeley. Previously, I was the first ML engineer at <a href="https://www.viaduct.ai/" target="_blank">Viaduct</a>, did research at <a href="https://ai.google/research/teams/brain" target="_blank">Google Brain</a>, and software engineering at <a href="https://www.facebook.com" target="_blank">Facebook</a>.
+          Currently, I am doing my PhD in databases at UC Berkeley. Previously, I was the first ML engineer at <a href="https://www.viaduct.ai/" target="_blank">Viaduct</a>, did research at <a href="https://ai.google/research/teams/brain" target="_blank">Google Brain</a>, and software engineering at <a href="https://www.facebook.com" target="_blank">Facebook</a>.
         </p>
         <p>
           I graduated from Stanford University with a B.S. and an M.S. in computer science with concentrations in systems and artificial intelligence, respectively. At Stanford, I helped run a nonprofit called SHE++, an organization that helps to empower underrepresented minorities in technology. I also spent a lot of time as a section leader for <a href="https://cs198.stanford.edu/cs198/Alumni.aspx" target="_blank">CS198</a> and served as the head TA for <a href="https://cs106b.stanford.edu" target="_blank">CS106B</a>.
