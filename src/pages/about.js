8f117813f5ef50df0e838f08951518ff5149b0a6
Shreya Shankar <ss.shankar505@gmail.com>
2022-07-23 18:46:58 -0500

    Adding advisor

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,7 +20,7 @@ export default class About extends React.Component {
         {/* <Image src={require('../images/profile-pic.jpg')} alt="Shreya Shankar" /> */}
         <h3>Bio</h3>
         <p>
-          I'm Shreya Shankar (she/her), a computer scientist living in San Francisco, California. Currently, I am doing my PhD in databases at UC Berkeley. Previously, I was the first ML engineer at <a href="https://www.viaduct.ai/" target="_blank">Viaduct</a>, did research at <a href="https://ai.google/research/teams/brain" target="_blank">Google Brain</a>, and software engineering at <a href="https://www.facebook.com" target="_blank">Facebook</a>.
+          I'm Shreya Shankar (she/her), a computer scientist living in San Francisco, California. Currently, I am doing my PhD in databases at UC Berkeley, advised by <a href="https://people.eecs.berkeley.edu/~adityagp/" target="_blank">Dr. Aditya Parameswaran</a>. Previously, I was the first ML engineer at <a href="https://www.viaduct.ai/" target="_blank">Viaduct</a>, did research at <a href="https://ai.google/research/teams/brain" target="_blank">Google Brain</a>, and software engineering at <a href="https://www.facebook.com" target="_blank">Facebook</a>.
         </p>
         <p>
           I graduated from Stanford University with a B.S. and an M.S. in computer science with concentrations in systems and artificial intelligence, respectively. At Stanford, I helped run a nonprofit called SHE++, an organization that helps to empower underrepresented minorities in technology. I also spent a lot of time as a section leader for <a href="https://cs198.stanford.edu/cs198/Alumni.aspx" target="_blank">CS198</a> and served as the head TA for <a href="https://cs106b.stanford.edu" target="_blank">CS106B</a>.
