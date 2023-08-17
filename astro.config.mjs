3a1f27e30ddc8c4deaad23fa646eafbc91871537
Shreya Shankar <ss.shankar505@gmail.com>
2023-08-17 12:46:50 -0700

    Trying SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://shreya-shankar.com',
 	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: netlify(),
 });
